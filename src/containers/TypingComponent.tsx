42bed90f90b7348702b52a1f247560b61c8a5329
Mitsuwo <hapowalp@gmail.com>
2020-12-28 05:10:29 +0900

    [add]タイピング実装

--- src/containers/TypingComponent.tsx
+++ src/containers/TypingComponent.tsx
@@ -6,16 +6,46 @@ interface Word {
   meaning: string;
 }
 
+const words: Word[] = JSON.parse(JSON.stringify(wordsJson));
+let { word } = words[Math.floor(Math.random() * words.length - 1)];
+
 const Typing: React.FC = () => {
-  const renderContent = (): string => {
-    const words: Word[] = JSON.parse(JSON.stringify(wordsJson));
-    const { word }: Word = words[Math.floor(Math.random() * words.length - 1)];
-    return word;
+  const [correctTypingConut, setCorrectTypingConut] = React.useState(0);
+  const containerRef: React.MutableRefObject<HTMLDivElement | null> = React.useRef(null);
+  const isElement = (element: HTMLDivElement | null): element is HTMLDivElement => {
+    return typeof element !== null;
+  };
+  React.useEffect(() => {
+    const { current } = containerRef;
+    if (isElement(current)) {
+      current.focus();
+    }
+  }, [setCorrectTypingConut]);
+  React.useEffect(() => {
+    if (correctTypingConut >= word.length) {
+      ({ word } = words[Math.floor(Math.random() * words.length - 1)]);
+      setCorrectTypingConut(0);
+    }
+  }, [correctTypingConut]);
+  const handleKeyDown = (event: React.KeyboardEvent): void => {
+    const { code } = event;
+    if (code.startsWith('Key') && word[correctTypingConut].toUpperCase() === code.substr(-1)) {
+      setCorrectTypingConut((count: number) => count + 1);
+    }
   };
   return (
-    <div>
-      <div>This is Typing.</div>
-      <div style={{ color: 'grey' }}>{renderContent()}</div>
+    <div ref={containerRef} onKeyDown={handleKeyDown} tabIndex={0}>
+      <div>Type below</div>
+      <div key={word}>
+        {word.split('').map((char: string, index: number) => {
+          const color = correctTypingConut > index ? 'black' : 'grey';
+          return (
+            <span style={{ color }} key={`${word}-${char}`}>
+              {char}
+            </span>
+          );
+        })}
+      </div>
     </div>
   );
 };
