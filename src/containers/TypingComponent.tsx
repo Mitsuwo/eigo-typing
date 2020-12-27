4688aab1f03fea398264b76af03decab8b10b250
Mitsuwo <hapowalp@gmail.com>
2020-12-27 23:28:55 +0900

    [add]文字列表示処理

--- src/containers/TypingComponent.tsx
+++ src/containers/TypingComponent.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import wordsJson from '../constant/words.json';
+
+interface Word {
+  word: string;
+  meaning: string;
+}
 
 const Typing: React.FC = () => {
-  return <div>This is Typing.</div>;
+  const renderContent = (): string => {
+    const words: Word[] = JSON.parse(JSON.stringify(wordsJson));
+    const { word }: Word = words[Math.floor(Math.random() * words.length - 1)];
+    return word;
+  };
+  return (
+    <div>
+      <div>This is Typing.</div>
+      <div style={{ color: 'grey' }}>{renderContent()}</div>
+    </div>
+  );
 };
 
 export const TypingComponent = Typing;
