0c1c57c70dcdf25a918733b8b13299fdcdca81cc
Mitsuwo <hapowalp@gmail.com>
2021-01-03 01:45:04 +0900

    [add]JSONから読み込んだ配列のシャッフル処理を追加

--- src/views/containers/Typing.tsx
+++ src/views/containers/Typing.tsx
@@ -1,20 +1,30 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { TypingConversation } from '../components/TypingConversation';
 import wordsJson from '../../constant/words.json';
 import { Word } from '../../store/TypingContent.tsx/types';
 import { setWords, setWord } from '../../store/TypingContent.tsx/action';
 import { RootState } from '../../store';
 import { setNextKey } from '../../store/Keyboard/actions';
 import { TypingWord } from './TypingWord';
+import { TypingConversation } from './TypingConversation';
 import { Keyboard } from './Keyboard';
 
 const TypingContainer: React.FC = () => {
   const dispatch = useDispatch();
   const { currentWordIndex } = useSelector((state: RootState) => state.typingContent);
   const { correctCharCount } = useSelector((state: RootState) => state.keyboard);
+  const shuffleArray = <T extends unknown>(array: T[]): T[] => {
+    for (let i = array.length - 1; 0 < i; i--) {
+      const randomNumber = Math.floor(Math.random() * (i + 1));
+      const obj = array[i];
+      array[i] = array[randomNumber];
+      array[randomNumber] = obj;
+    }
+    return array;
+  };
   React.useEffect(() => {
-    const words: Word[] = JSON.parse(JSON.stringify(wordsJson));
+    let words: Word[] = JSON.parse(JSON.stringify(wordsJson));
+    words = shuffleArray<Word>(words);
     const { word } = words[currentWordIndex];
     dispatch(setWord(word));
     dispatch(setWords(words));
