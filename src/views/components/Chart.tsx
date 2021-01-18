c71288a09b278cc0659a40bb3b2cf5f6b07b5855
Mitsuwo <hapowalp@gmail.com>
2021-01-18 23:05:19 +0900

    [add]棒グラフ追加

---
+++ src/views/components/Chart.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+import { Interval } from '../../store/PageManager/types';
+
+type Props = {
+  data: Interval[];
+};
+
+const ChartComponent: React.FC<Props> = (props: Props) => {
+  return (
+    <BarChart
+      width={500}
+      height={300}
+      data={props.data}
+      margin={{
+        top: 5,
+        right: 30,
+        left: 20,
+        bottom: 5
+      }}>
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="key" />
+      <YAxis />
+      <Tooltip />
+      <Legend />
+      <Bar dataKey="interval" fill="#82ca9d" />
+    </BarChart>
+  );
+};
+
+export const Chart = React.memo(ChartComponent);
--- src/views/containers/Result.tsx
+++ src/views/containers/Result.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../store';
+import { Chart } from '../components/Chart';
 
 const ResultContainer: React.FC = () => {
+  const { intervals } = useSelector((state: RootState) => state.pageManager);
   return (
     <div>
       <div>Result Page</div>
+      <Chart data={intervals} />
     </div>
   );
 };
