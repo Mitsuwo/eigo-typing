8ee8ccb795f9eb017fcc41281ab7e491f59a1ea6
Mitsuwo <hapowalp@gmail.com>
2021-01-20 23:31:49 +0900

    [add]棒グラフ追加

--- src/views/containers/Result.tsx
+++ src/views/containers/Result.tsx
@@ -1,14 +1,30 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../store';
+import { Interval } from '../../store/PageManager/types';
 import { Chart } from '../components/Chart';
 
 const ResultContainer: React.FC = () => {
   const { intervals } = useSelector((state: RootState) => state.pageManager);
+  React.useEffect(() => {
+    const data: Interval[] = [];
+    intervals.forEach((interval: Interval) => {
+      const sameKeyIndex = data.findIndex(
+        (addedInterval: Interval) => addedInterval.key === interval.key
+      );
+      if (sameKeyIndex === -1) {
+        data.push(interval);
+      } else {
+        data[sameKeyIndex].interval = (data[sameKeyIndex].interval + interval.interval) / 2;
+      }
+    });
+    setDataState(data);
+  }, []);
+  const [dataState, setDataState] = React.useState<Interval[]>([]);
   return (
     <div>
       <div>Result Page</div>
-      <Chart data={intervals} />
+      <Chart data={dataState} />
     </div>
   );
 };
