bb8af0354403d6780b010f79a6cfff77d501bdd6
Mitsuwo <hapowalp@gmail.com>
2021-02-01 20:41:50 +0900

    [add]タイピングアニメーション追加

--- src/views/components/AnimatedTyping.tsx
+++ src/views/components/AnimatedTyping.tsx
@@ -1,22 +1,32 @@
 import React from 'react';
 import Typing from 'react-typing-animation';
+import styled from 'styled-components';
 
 const AnimatedTypingComponent: React.FC = () => {
   return (
-    <div style={{ marginTop: '10vh', height: '25vh', textAlign: 'center' }}>
-      <Typing>
-        <div style={{ color: '#e1eef6', fontSize: '25px' }}>
-          This is English Typing Practice.
-          <Typing.Delay ms={1000} />
+    <StyledAnimatedTyping>
+      <Typing cursorClassName="cursor">
+        <div style={{ color: '#e1eef6', fontSize: '3vh', fontFamily: 'oxygenMono' }}>
+          <span>This is English Typing Practice.</span>
+          <Typing.Delay ms={2000} />
           <br />
-          You can improve your typing speed and learn English.
-          <Typing.Delay ms={1000} />
+          <span>You can improve your typing speed and learn English.</span>
+          <Typing.Delay ms={2000} />
           <br />
-          Please click the START button below.
+          <span>Please click the START button below.</span>
         </div>
       </Typing>
-    </div>
+    </StyledAnimatedTyping>
   );
 };
+
+const StyledAnimatedTyping = styled.div`
+  margin-top: 10vh;
+  height: 20vh;
+  text-align: center;
+  .cursor {
+    color: #e1eef6;
+  }
+`;
 
 export const AnimatedTyping = React.memo(AnimatedTypingComponent);
--- src/views/containers/Home.tsx
+++ src/views/containers/Home.tsx
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { RouteComponentProps, withRouter } from 'react-router';
 import { Button } from '@material-ui/core';
 import styled from 'styled-components';
+import bgImage from '../../images/bg.jpg';
 import { RootState } from '../../store';
 import { resetPageManager, setAppState } from '../../store/PageManager/actions';
 import { APP_STATE_TYPING } from '../../store/PageManager/types';
@@ -26,16 +27,25 @@ const HomeContainer: React.FC<RouteComponentProps> = (props: RouteComponentProps
   };
   return (
     <div
-      style={{ height: '100vh', width: '100vw', backgroundColor: '#f9c00c', paddingTop: '25vh' }}>
+      style={{
+        height: '100vh',
+        width: '100vw',
+        backgroundImage: `url(${bgImage})`,
+        backgroundSize: '100% 100%',
+        overflowY: 'hidden'
+      }}>
       <AppTitle>えいごでタイピング</AppTitle>
       <AnimatedTyping />
       <CustomThemeProvider>
         <ButtonParent>
           <Button variant="contained" color="secondary" onClick={linkToTyping}>
-            <p style={{ color: '#e1eef6', fontSize: '20px', margin: 0 }}>START</p>
+            <p style={{ color: '#e1eef6', fontFamily: 'oxygenMono', fontSize: '20px', margin: 0 }}>
+              START
+            </p>
           </Button>
           <ShowJapaneseCheckBox
             showJapanese={showJapanese}
+            fontColor="#e1eef6"
             switchShowJapanese={switchShowJapanese}
           />
         </ButtonParent>
@@ -45,6 +55,9 @@ const HomeContainer: React.FC<RouteComponentProps> = (props: RouteComponentProps
 };
 
 const AppTitle = styled.div`
+  height: 5vh;
+  margin-top: 15vh;
+  margin-bottom: 5vh;
   color: #e1eef6;
   font-size: 40px;
   text-align: center;
