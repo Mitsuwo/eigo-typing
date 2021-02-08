5ac9afde7aabbf268e52b4194880824e152b2089
Mitsuwo <hapowalp@gmail.com>
2021-02-08 22:41:10 +0900

    [add]jest

---
+++ babel.config.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-undef */
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        modules: 'false',
+        useBuiltIns: 'usage',
+        targets: '> 0.25%, not dead'
+      }
+    ]
+  ],
+  env: {
+    test: {
+      presets: [['@babel/preset-env', { targets: { node: 'current' } }]]
+    }
+  }
+};
---
+++ jest.config.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-undef */
+module.exports = {
+  verbose: true,
+  moduleFileExtensions: ['ts', 'js'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest'
+  },
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.json'
+    }
+  },
+  testMatch: ['**/test/**/*.test.ts']
+};
